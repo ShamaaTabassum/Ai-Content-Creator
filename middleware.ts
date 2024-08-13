51c0189949ebab95fadf85ee8ffaf30ac1caaae4
Shama Tabassum <girlyside76@gmail.com>
2024-08-13 12:20:53 +0500

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import {
     createRouteMatcher
   } from '@clerk/nextjs/server';
   
-  const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+  const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up']);
 
   export default clerkMiddleware((auth, request) => {
     if(!isPublicRoute(request)) {
